55586aab823064c96e4d184e631f9d101c01ae4e
shehzad1khan <commonemail61@gmail.com>
2026-09-08 15:51:48 +0500

    push from office

---
+++ simpleCrud/resources/views/demo.blade.php
@@ -0,0 +1,4 @@
+<h1>Hello this is Demo page</h1>
+
+<h3>Your name is {{ $name }}</h3>
+<h4>And Your Id is {{ $id }}</h4>
--- simpleCrud/routes/web.php
+++ simpleCrud/routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', function(){
+    echo"Hello World";
+});
+
+Route::get('demo/{name?}/{id?}', function($name='Aladeen', $id='not define'){
+    $data = compact('name', 'id');
+    return view('demo')->with($data);
+});
+
+// video number 6, duration = 10:00;
